03991e81c451522ce78b42d3b43b54ba306ab02e
Brent Komer <brent.komer@gmail.com>
2019-05-08 17:06:46 -0400

    more work on localization code

---
+++ localization/localization_training_utils.py
@@ -0,0 +1,330 @@
+import numpy as np
+from spatial_semantic_pointers.utils import encode_point, encode_random, ssp_to_loc, ssp_to_loc_v
+from spatial_semantic_pointers.plots import plot_predictions, plot_predictions_v
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+import matplotlib.pyplot as plt
+
+
+class ValidationSet(object):
+
+    def __init__(self, dataloader, heatmap_vectors, xs, ys, ssp_scaling=5, spatial_encoding='ssp'):
+
+        self.dataloader = dataloader
+        self.heatmap_vectors = heatmap_vectors
+        self.xs = xs
+        self.ys = ys
+        self.ssp_scaling = ssp_scaling
+        self.spatial_encoding = spatial_encoding
+        self.cosine_criterion = nn.CosineEmbeddingLoss()
+        self.mse_criterion = nn.MSELoss()
+
+    def run_eval(self, model, writer, epoch):
+
+        with torch.no_grad():
+            # Everything is in one batch, so this loop will only happen once
+            for i, data in enumerate(self.dataloader):
+                velocity_inputs, sensor_inputs, ssp_inputs, ssp_outputs = data
+
+                ssp_pred = model(velocity_inputs, sensor_inputs, ssp_inputs)
+
+                # NOTE: need to permute axes of the targets here because the output is
+                #       (sequence length, batch, units) instead of (batch, sequence_length, units)
+                #       could also permute the outputs instead
+                cosine_loss = self.cosine_criterion(ssp_pred, ssp_outputs.permute(1, 0, 2),
+                                 torch.ones(ssp_pred.shape[0], ssp_pred.shape[0]))
+                mse_loss = self.mse_criterion(ssp_pred, ssp_outputs.permute(1, 0, 2))
+
+                print("test mse loss", mse_loss.data.item())
+                print("test cosine loss", mse_loss.data.item())
+
+            writer.add_scalar('test_mse_loss', mse_loss.data.item(), epoch)
+            writer.add_scalar('test_cosine_loss', cosine_loss.data.item(), epoch)
+
+            # Just use start and end location to save on memory and computation
+            predictions_start = np.zeros((ssp_pred.shape[1], 2))
+            coords_start = np.zeros((ssp_pred.shape[1], 2))
+
+            predictions_end = np.zeros((ssp_pred.shape[1], 2))
+            coords_end = np.zeros((ssp_pred.shape[1], 2))
+
+            if self.spatial_encoding == 'ssp':
+                print("computing prediction locations")
+                predictions_start[:, :] = ssp_to_loc_v(
+                    ssp_pred.detach().numpy()[0, :, :],
+                    self.heatmap_vectors, self.xs, self.ys
+                )
+                predictions_end[:, :] = ssp_to_loc_v(
+                    ssp_pred.detach().numpy()[-1, :, :],
+                    self.heatmap_vectors, self.xs, self.ys
+                )
+                print("computing ground truth locations")
+                coords_start[:, :] = ssp_to_loc_v(
+                    ssp_outputs.detach().numpy()[:, 0, :],
+                    self.heatmap_vectors, self.xs, self.ys
+                )
+                coords_end[:, :] = ssp_to_loc_v(
+                    ssp_outputs.detach().numpy()[:, -1, :],
+                    self.heatmap_vectors, self.xs, self.ys
+                )
+            elif self.spatial_encoding == '2d':
+                print("copying prediction locations")
+                predictions_start[:, :] = ssp_pred.detach().numpy()[0, :, :]
+                predictions_end[:, :] = ssp_pred.detach().numpy()[-1, :, :]
+                print("copying ground truth locations")
+                coords_start[:, :] = ssp_outputs.detach().numpy()[:, 0, :]
+                coords_end[:, :] = ssp_outputs.detach().numpy()[:, -1, :]
+
+            fig_pred_start, ax_pred_start = plt.subplots()
+            fig_truth_start, ax_truth_start = plt.subplots()
+            fig_pred_end, ax_pred_end = plt.subplots()
+            fig_truth_end, ax_truth_end = plt.subplots()
+
+            print("plotting predicted locations")
+            plot_predictions_v(
+                predictions_start / self.ssp_scaling,
+                coords_start / self.ssp_scaling,
+                ax_pred_start,
+                min_val=0,
+                max_val=2.2
+            )
+            plot_predictions_v(
+                predictions_end / self.ssp_scaling,
+                coords_end / self.ssp_scaling,
+                ax_pred_end,
+                min_val=0,
+                max_val=2.2
+            )
+            print("plotting ground truth locations")
+            plot_predictions_v(
+                coords_start / self.ssp_scaling,
+                coords_start / self.ssp_scaling,
+                ax_truth_start,
+                min_val=0,
+                max_val=2.2
+            )
+            plot_predictions_v(
+                coords_end / self.ssp_scaling,
+                coords_end / self.ssp_scaling,
+                ax_truth_end,
+                min_val=0,
+                max_val=2.2
+            )
+
+            writer.add_figure("predictions start", fig_pred_start, epoch)
+            writer.add_figure("ground truth start", fig_truth_start, epoch)
+
+            writer.add_figure("predictions end", fig_pred_end, epoch)
+            writer.add_figure("ground truth end", fig_truth_end, epoch)
+
+
+class LocalizationTrajectoryDataset(data.Dataset):
+
+    def __init__(self, velocity_inputs, sensor_inputs, ssp_inputs, ssp_outputs, return_velocity_list=True):
+
+        self.velocity_inputs = velocity_inputs.astype(np.float32)
+        self.sensor_inputs = sensor_inputs.astype(np.float32)
+        self.combined_inputs = np.hstack([self.velocity_inputs, self.sensor_inputs])
+        assert (self.velocity_inputs.shape[0] == self.combined_inputs.shape[0])
+        assert (self.velocity_inputs.shape[1] == self.combined_inputs.shape[1])
+        self.ssp_inputs = ssp_inputs.astype(np.float32)
+        self.ssp_outputs = ssp_outputs.astype(np.float32)
+
+        # flag for whether the velocities returned are a single tensor or a list of tensors
+        self.return_velocity_list = return_velocity_list
+
+    def __getitem__(self, index):
+
+        if self.return_velocity_list:
+            return [self.combined_inputs[index, i] for i in range(self.combined_inputs.shape[1])], \
+                   self.ssp_inputs[index], self.ssp_outputs[index],
+        else:
+            return self.combined_inputs[index], self.ssp_inputs[index], self.ssp_outputs[index]
+
+    def __len__(self):
+        return self.combined_inputs.shape[0]
+
+
+def localization_train_test_loaders(data, n_train_samples=1000, n_test_samples=1000, rollout_length=100, batch_size=10, encoding='ssp'):
+
+    # Option to use SSPs or the 2D location directly
+    assert encoding in ['ssp', '2d', 'pc']
+
+    positions = data['positions']
+
+    dist_sensors = data['dist_sensors']
+    n_sensors = dist_sensors.shape[2]
+
+    cartesian_vels = data['cartesian_vels']
+    ssps = data['ssps']
+    n_place_cells = data['pc_centers'].shape[0]
+
+    pc_activations = data['pc_activations']
+
+    n_trajectories = positions.shape[0]
+    trajectory_length = positions.shape[1]
+    dim = ssps.shape[2]
+
+    for test_set, n_samples in enumerate([n_train_samples, n_test_samples]):
+
+        velocity_inputs = np.zeros((n_samples, rollout_length, 2))
+
+        sensor_inputs = np.zeros((n_samples, rollout_length, n_sensors))
+
+        # these include outputs for every time-step
+        ssp_outputs = np.zeros((n_samples, rollout_length, dim))
+
+        ssp_inputs = np.zeros((n_samples, dim))
+
+        # for the 2D encoding method
+        pos_outputs = np.zeros((n_samples, rollout_length, 2))
+
+        pos_inputs = np.zeros((n_samples, 2))
+
+        # for the place cell encoding method
+        pc_outputs = np.zeros((n_samples, rollout_length, n_place_cells))
+
+        pc_inputs = np.zeros((n_samples, n_place_cells))
+
+        for i in range(n_samples):
+            # choose random trajectory
+            traj_ind = np.random.randint(low=0, high=n_trajectories)
+            # choose random segment of trajectory
+            step_ind = np.random.randint(low=0, high=trajectory_length - rollout_length - 1)
+
+            # index of final step of the trajectory
+            step_ind_final = step_ind + rollout_length
+
+            velocity_inputs[i, :, :] = cartesian_vels[traj_ind, step_ind:step_ind_final, :]
+
+            sensor_inputs[i, :, :] = dist_sensors[traj_ind, step_ind:step_ind_final, :]
+
+            # ssp output is shifted by one timestep (since it is a prediction of the future by one step)
+            ssp_outputs[i, :, :] = ssps[traj_ind, step_ind + 1:step_ind_final + 1, :]
+            # initial state of the LSTM is a linear transform of the ground truth ssp
+            ssp_inputs[i, :] = ssps[traj_ind, step_ind]
+
+            # for the 2D encoding method
+            pos_outputs[i, :, :] = positions[traj_ind, step_ind + 1:step_ind_final + 1, :]
+            pos_inputs[i, :] = positions[traj_ind, step_ind]
+
+            # for the place cell encoding method
+            pc_outputs[i, :, :] = pc_activations[traj_ind, step_ind + 1:step_ind_final + 1, :]
+            pc_inputs[i, :] = pc_activations[traj_ind, step_ind]
+
+        if encoding == 'ssp':
+            dataset = LocalizationTrajectoryDataset(
+                velocity_inputs=velocity_inputs,
+                sensor_inputs=sensor_inputs,
+                ssp_inputs=ssp_inputs,
+                ssp_outputs=ssp_outputs,
+            )
+        elif encoding == '2d':
+            dataset = LocalizationTrajectoryDataset(
+                velocity_inputs=velocity_inputs,
+                sensor_inputs=sensor_inputs,
+                ssp_inputs=pos_inputs,
+                ssp_outputs=pos_outputs,
+            )
+        elif encoding == 'pc':
+            dataset = LocalizationTrajectoryDataset(
+                velocity_inputs=velocity_inputs,
+                sensor_inputs=sensor_inputs,
+                ssp_inputs=pc_inputs,
+                ssp_outputs=pc_outputs,
+            )
+
+        if test_set == 0:
+            trainloader = torch.utils.data.DataLoader(
+                dataset, batch_size=batch_size, shuffle=True, num_workers=0,
+            )
+        elif test_set == 1:
+            testloader = torch.utils.data.DataLoader(
+                dataset, batch_size=n_samples, shuffle=True, num_workers=0,
+            )
+
+    return trainloader, testloader
+
+
+class LocalizationModel(nn.Module):
+
+    def __init__(self, input_size, lstm_hidden_size=128, linear_hidden_size=512,
+                 unroll_length=100, sp_dim=512,):
+
+        super(LocalizationModel, self).__init__()
+
+        self.input_size = input_size  # velocity and sensor measurements
+        self.lstm_hidden_size = lstm_hidden_size
+        self.linear_hidden_size = linear_hidden_size
+        self.unroll_length = unroll_length
+
+        self.sp_dim = sp_dim
+
+        # Full LSTM that can be given the full sequence and produce the full output in one step
+        self.lstm = nn.LSTM(
+            input_size=self.input_size,
+            hidden_size=self.lstm_hidden_size,
+            num_layers=1
+        )
+
+        self.linear = nn.Linear(
+            in_features=self.lstm_hidden_size,
+            out_features=self.linear_hidden_size,
+        )
+
+        self.dropout = nn.Dropout(p=.5)
+
+        self.ssp_output = nn.Linear(
+            in_features=self.linear_hidden_size,
+            out_features=self.sp_dim
+        )
+
+        # Linear transforms for ground truth ssp into initial hidden and cell state of lstm
+        self.w_c = nn.Linear(
+            in_features=self.sp_dim,
+            out_features=self.lstm_hidden_size,
+            bias=False,
+        )
+
+        self.w_h = nn.Linear(
+            in_features=self.sp_dim,
+            out_features=self.lstm_hidden_size,
+            bias=False,
+        )
+
+    def forward(self, inputs, initial_ssp):
+        """
+        :param inputs: contains both velocity and distance sensor measurments
+        :param initial_ssp: SSP ground truth for the start location of the trajectory
+        :return: predicted SSP for agent location at the end of the trajectory
+        """
+
+        ssp_pred, output = self.forward_activations(inputs, initial_ssp)
+        return ssp_pred
+
+    def forward_activations(self, inputs, initial_ssp):
+        """Returns the hidden layer activations as well as the prediction"""
+
+        batch_size = inputs[0].shape[0]
+
+        # Compute initial hidden state
+        cell_state = self.w_c(initial_ssp)
+        hidden_state = self.w_h(initial_ssp)
+
+        vel_sense_inputs = torch.cat(inputs).view(len(inputs), batch_size, -1)
+
+        output, (_, _) = self.lstm(
+            vel_sense_inputs,
+            (
+                hidden_state.view(1, batch_size, self.lstm_hidden_size),
+                cell_state.view(1, batch_size, self.lstm_hidden_size)
+            )
+        )
+
+        features = self.dropout(self.linear(output))
+
+        # TODO: should normalization be used here?
+        ssp_pred = self.ssp_output(features)
+
+        return ssp_pred, output
--- localization/train_localization.py
+++ localization/train_localization.py
@@ -13,8 +13,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from datasets import localization_train_test_loaders
-from models import SSPPathIntegrationModel
 from datetime import datetime
 from tensorboardX import SummaryWriter
 import json
@@ -22,6 +20,7 @@ from spatial_semantic_pointers.utils import get_heatmap_vectors, ssp_to_loc, ssp
 from spatial_semantic_pointers.plots import plot_predictions, plot_predictions_v
 import matplotlib.pyplot as plt
 from path_integration_utils import pc_to_loc_v
+from localization_training_utils import ValidationSet, localization_train_test_loaders, LocalizationModel
 
 
 parser = argparse.ArgumentParser('Run 2D supervised localization experiment using pytorch')
@@ -93,11 +92,9 @@ batch_size = args.minibatch_size#10
 n_epochs = args.n_epochs#20
 # n_epochs = 5
 
-
-
 # Input is x and y velocity plus the distance sensor measurements
-model = SSPPathIntegrationModel(
-    inputs_size=2 + n_sensors,
+model = LocalizationModel(
+    input_size=2 + n_sensors,
     unroll_length=rollout_length,
     sp_dim=dim
 )
@@ -125,6 +122,15 @@ trainloader, testloader = localization_train_test_loaders(
     encoding=args.encoding,
 )
 
+validation_set = ValidationSet(
+    dataloader=testloader,
+    heatmap_vectors=heatmap_vectors,
+    xs=xs,
+    ys=ys,
+    ssp_scaling=ssp_scaling,
+    spatial_encoding=args.encoding,
+)
+
 params = vars(args)
 with open(os.path.join(save_dir, "params.json"), "w") as f:
     json.dump(params, f)
@@ -133,116 +139,21 @@ print("Training")
 for epoch in range(n_epochs):
     print("Epoch {} of {}".format(epoch + 1, n_epochs))
 
-
-    # TODO: modularize this and clean it up ++
     # Every 'eval_period' epochs, create a test loss and image
     if epoch % args.eval_period == 0:
+
         print("Evaluating")
-        with torch.no_grad():
-            # Everything is in one batch, so this loop will only happen once
-            for i, data in enumerate(testloader):
-                velocity_inputs, ssp_inputs, ssp_outputs = data
+        validation_set.run_eval(
+            model=model,
+            writer=writer,
+            epoch=epoch,
+        )
 
-                ssp_pred = model(velocity_inputs, ssp_inputs)
-
-                # NOTE: need to permute axes of the targets here because the output is
-                #       (sequence length, batch, units) instead of (batch, sequence_length, units)
-                #       could also permute the outputs instead
-                if args.encoding == 'pc':
-                    # place cell version needs to explicitly do the softmax here
-                    loss = criterion(ssp_pred, F.softmax(ssp_outputs.permute(1, 0, 2), dim=2))
-                else:
-                    cosine_loss = criterion(ssp_pred, ssp_outputs.permute(1, 0, 2),
-                                     torch.ones(ssp_pred.shape[0], ssp_pred.shape[0]))
-                    mse_loss = criterion(ssp_pred, ssp_outputs.permute(1, 0, 2))
-
-                print("test loss", loss.data.item())
-
-            writer.add_scalar('test_loss', loss.data.item(), epoch)
-
-            print("ssp_pred.shape", ssp_pred.shape)
-            print("ssp_outputs.shape", ssp_outputs.shape)
-
-            # Just use start and end location to save on memory and computation
-            predictions_start = np.zeros((ssp_pred.shape[1], 2))
-            coords_start = np.zeros((ssp_pred.shape[1], 2))
-
-            predictions_end = np.zeros((ssp_pred.shape[1], 2))
-            coords_end = np.zeros((ssp_pred.shape[1], 2))
-
-            if args.encoding == 'ssp':
-                print("computing prediction locations")
-                predictions_start[:, :] = ssp_to_loc_v(
-                    ssp_pred.detach().numpy()[0, :, :],
-                    heatmap_vectors, xs, ys
-                )
-                predictions_end[:, :] = ssp_to_loc_v(
-                    ssp_pred.detach().numpy()[-1, :, :],
-                    heatmap_vectors, xs, ys
-                )
-                print("computing ground truth locations")
-                coords_start[:, :] = ssp_to_loc_v(
-                    ssp_outputs.detach().numpy()[:, 0, :],
-                    heatmap_vectors, xs, ys
-                )
-                coords_end[:, :] = ssp_to_loc_v(
-                    ssp_outputs.detach().numpy()[:, -1, :],
-                    heatmap_vectors, xs, ys
-                )
-            elif args.encoding == '2d':
-                print("copying prediction locations")
-                predictions_start[:, :] = ssp_pred.detach().numpy()[0, :, :]
-                predictions_end[:, :] = ssp_pred.detach().numpy()[-1, :, :]
-                print("copying ground truth locations")
-                coords_start[:, :] = ssp_outputs.detach().numpy()[:, 0, :]
-                coords_end[:, :] = ssp_outputs.detach().numpy()[:, -1, :]
-            elif args.encoding == 'pc':
-                # (quick hack is to just use the most activated place cell center)
-                predictions_start[:, :] = pc_to_loc_v(
-                    pc_activations=ssp_pred.detach().numpy()[0, :, :],
-                    centers=pc_centers,
-                    jitter=0.01
-                )
-                predictions_end[:, :] = pc_to_loc_v(
-                    pc_activations=ssp_pred.detach().numpy()[-1, :, :],
-                    centers=pc_centers,
-                    jitter=0.01
-                )
-
-                coords_start[:, :] = pc_to_loc_v(
-                    pc_activations=ssp_outputs.detach().numpy()[:, 0, :],
-                    centers=pc_centers,
-                    jitter=0.01
-                )
-                coords_end[:, :] = pc_to_loc_v(
-                    pc_activations=ssp_outputs.detach().numpy()[:, -1, :],
-                    centers=pc_centers,
-                    jitter=0.01
-                )
-
-            fig_pred_start, ax_pred_start = plt.subplots()
-            fig_truth_start, ax_truth_start = plt.subplots()
-            fig_pred_end, ax_pred_end = plt.subplots()
-            fig_truth_end, ax_truth_end = plt.subplots()
-
-            print("plotting predicted locations")
-            plot_predictions_v(predictions_start / ssp_scaling, coords_start / ssp_scaling, ax_pred_start, min_val=0, max_val=2.2)
-            plot_predictions_v(predictions_end / ssp_scaling, coords_end / ssp_scaling, ax_pred_end, min_val=0, max_val=2.2)
-            print("plotting ground truth locations")
-            plot_predictions_v(coords_start / ssp_scaling, coords_start / ssp_scaling, ax_truth_start, min_val=0, max_val=2.2)
-            plot_predictions_v(coords_end / ssp_scaling, coords_end / ssp_scaling, ax_truth_end, min_val=0, max_val=2.2)
-
-            writer.add_figure("predictions start", fig_pred_start, epoch)
-            writer.add_figure("ground truth start", fig_truth_start, epoch)
-
-            writer.add_figure("predictions end", fig_pred_end, epoch)
-            writer.add_figure("ground truth end", fig_truth_end, epoch)
-
-
-    avg_loss = 0
+    avg_mse_loss = 0
+    avg_cosine_loss = 0
     n_batches = 0
     for i, data in enumerate(trainloader):
-        velocity_inputs, ssp_inputs, ssp_outputs = data
+        velocity_inputs, sensor_inputs, ssp_inputs, ssp_outputs = data
 
         if ssp_inputs.size()[0] != batch_size:
             continue  # Drop data, not enough for a batch
@@ -272,120 +183,24 @@ for epoch in range(n_epochs):
 
         optimizer.step()
 
-        avg_loss += loss.data.item()
+        avg_mse_loss += mse_loss.data.item()
+        avg_cosine_loss += cosine_loss.data.item()
         n_batches += 1
 
-    avg_loss /= n_batches
-    print("loss:", avg_loss)
-    writer.add_scalar('avg_loss', avg_loss, epoch + 1)
-
-
+    avg_mse_loss /= n_batches
+    avg_cosine_loss /= n_batches
+    print("mse loss:", avg_mse_loss)
+    print("cosine loss:", avg_cosine_loss)
+    writer.add_scalar('avg_mse_loss', avg_mse_loss, epoch + 1)
+    writer.add_scalar('avg_cosine_loss', avg_cosine_loss, epoch + 1)
 
 
 print("Testing")
-with torch.no_grad():
-    # Everything is in one batch, so this loop will only happen once
-    for i, data in enumerate(testloader):
-        velocity_inputs, ssp_inputs, ssp_outputs = data
-
-        ssp_pred = model(velocity_inputs, ssp_inputs)
-
-        # NOTE: need to permute axes of the targets here because the output is
-        #       (sequence length, batch, units) instead of (batch, sequence_length, units)
-        #       could also permute the outputs instead
-        if args.encoding == 'pc':
-            # place cell version needs to explicitly do the softmax here
-            loss = criterion(ssp_pred, F.softmax(ssp_outputs.permute(1, 0, 2), dim=2))
-        else:
-            if args.use_cosine_loss:
-                loss = criterion(ssp_pred, ssp_outputs.permute(1, 0, 2),
-                                 torch.ones(ssp_pred.shape[0], ssp_pred.shape[0]))
-            else:
-                loss = criterion(ssp_pred, ssp_outputs.permute(1, 0, 2))
-
-        print("test loss", loss.data.item())
-
-    writer.add_scalar('final_test_loss', loss.data.item())
-
-    print("ssp_pred.shape", ssp_pred.shape)
-    print("ssp_outputs.shape", ssp_outputs.shape)
-
-    # Just use start and end location to save on memory and computation
-    predictions_start = np.zeros((ssp_pred.shape[1], 2))
-    coords_start = np.zeros((ssp_pred.shape[1], 2))
-
-    predictions_end = np.zeros((ssp_pred.shape[1], 2))
-    coords_end = np.zeros((ssp_pred.shape[1], 2))
-
-    if args.encoding == 'ssp':
-        print("computing prediction locations")
-        predictions_start[:, :] = ssp_to_loc_v(
-            ssp_pred.detach().numpy()[0, :, :],
-            heatmap_vectors, xs, ys
-        )
-        predictions_end[:, :] = ssp_to_loc_v(
-            ssp_pred.detach().numpy()[-1, :, :],
-            heatmap_vectors, xs, ys
-        )
-        print("computing ground truth locations")
-        coords_start[:, :] = ssp_to_loc_v(
-            ssp_outputs.detach().numpy()[:, 0, :],
-            heatmap_vectors, xs, ys
-        )
-        coords_end[:, :] = ssp_to_loc_v(
-            ssp_outputs.detach().numpy()[:, -1, :],
-            heatmap_vectors, xs, ys
-        )
-    elif args.encoding == '2d':
-        print("copying prediction locations")
-        predictions_start[:, :] = ssp_pred.detach().numpy()[0, :, :]
-        predictions_end[:, :] = ssp_pred.detach().numpy()[-1, :, :]
-        print("copying ground truth locations")
-        coords_start[:, :] = ssp_outputs.detach().numpy()[:, 0, :]
-        coords_end[:, :] = ssp_outputs.detach().numpy()[:, -1, :]
-    elif args.encoding == 'pc':
-        # (quick hack is to just use the most activated place cell center)
-        predictions_start[:, :] = pc_to_loc_v(
-            pc_activations=ssp_pred.detach().numpy()[0, :, :],
-            centers=pc_centers,
-            jitter=0.01
-        )
-        predictions_end[:, :] = pc_to_loc_v(
-            pc_activations=ssp_pred.detach().numpy()[-1, :, :],
-            centers=pc_centers,
-            jitter=0.01
-        )
-
-        coords_start[:, :] = pc_to_loc_v(
-            pc_activations=ssp_outputs.detach().numpy()[:, 0, :],
-            centers=pc_centers,
-            jitter=0.01
-        )
-        coords_end[:, :] = pc_to_loc_v(
-            pc_activations=ssp_outputs.detach().numpy()[:, -1, :],
-            centers=pc_centers,
-            jitter=0.01
-        )
-
-    fig_pred_start, ax_pred_start = plt.subplots()
-    fig_truth_start, ax_truth_start = plt.subplots()
-    fig_pred_end, ax_pred_end = plt.subplots()
-    fig_truth_end, ax_truth_end = plt.subplots()
-
-    print("plotting predicted locations")
-    plot_predictions_v(predictions_start / ssp_scaling, coords_start / ssp_scaling, ax_pred_start, min_val=0, max_val=2.2)
-    plot_predictions_v(predictions_end / ssp_scaling, coords_end / ssp_scaling, ax_pred_end, min_val=0, max_val=2.2)
-    print("plotting ground truth locations")
-    plot_predictions_v(coords_start / ssp_scaling, coords_start / ssp_scaling, ax_truth_start, min_val=0, max_val=2.2)
-    plot_predictions_v(coords_end / ssp_scaling, coords_end / ssp_scaling, ax_truth_end, min_val=0, max_val=2.2)
-
-    writer.add_figure("final predictions start", fig_pred_start)
-    writer.add_figure("final ground truth start", fig_truth_start)
-
-    writer.add_figure("final predictions end", fig_pred_end)
-    writer.add_figure("final ground truth end", fig_truth_end)
-
-
+validation_set.run_eval(
+    model=model,
+    writer=writer,
+    epoch=n_epochs,
+)
 
 if args.encoding == 'ssp':
     torch.save(model.state_dict(), os.path.join(save_dir, 'ssp_localization_model.pt'))
