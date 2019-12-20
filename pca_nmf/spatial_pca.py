4d95960c054720418ece5c4c592f262bcc5d89ea
Brent Komer <brent.komer@gmail.com>
2019-12-19 23:10:39 -0500

    playing around with covariance sorting

--- pca_nmf/spatial_pca.py
+++ pca_nmf/spatial_pca.py
@@ -1,5 +1,6 @@
 # compute PCA on the place cell firing from trajectories of an agent moving through an environment
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.decomposition import PCA, NMF
 import numpy as np
 import argparse
@@ -37,11 +38,28 @@ encoding_func, dim = get_encoding_function(args, limit_low=0, limit_high=2.2)
 activations, flat_pos = get_activations(data=data, encoding_func=encoding_func, encoding_dim=dim)
 
 pca = PCA(n_components=args.n_components)
+# pca = NMF(n_components=args.n_components)
 
 print("Fitting PCA")
 pca.fit(activations)
 
-print(pca.singular_values_)
+print("Getting covariance")
+covariance = pca.get_covariance()
+
+print("Plotting covariance")
+plt.figure()
+plt.imshow(covariance)
+
+# sns.clustermap(covariance, metric='correlation', method='centroid')
+# sns.clustermap(covariance, metric='euclidean', method='centroid')
+# sns.clustermap(covariance, metric='euclidean')
+# sns.clustermap(covariance, metric='correlation')
+sns.clustermap(covariance, metric='correlation', method='weighted')
+sns.clustermap(covariance, metric='correlation', method='complete')
+
+plt.show()
+
+# print(pca.singular_values_)
 
 print("Transforming with PCA")
 transformed_activations = pca.transform(activations)
@@ -52,7 +70,7 @@ print(transformed_activations.shape)
 print("computing heatmap")
 xs = np.linspace(0, 2.2, args.res)
 ys = np.linspace(0, 2.2, args.res)
-heatmap = spatial_heatmap(activations, flat_pos, xs, ys)
+heatmap = spatial_heatmap(transformed_activations, flat_pos, xs, ys)
 
 for n in range(args.n_components):
     plt.figure()
