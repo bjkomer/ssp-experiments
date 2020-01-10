3ee026a9645d887071ec0a2eb25b52bee58a8e7a
Brent Komer <brent.komer@gmail.com>
2020-01-10 17:02:16 -0500

    scripts for viewing effects of axis vector generation type

--- axis_vector_explorations/averaged_nd_heatmaps.py
+++ axis_vector_explorations/averaged_nd_heatmaps.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import argparse
 import os
 from spatial_semantic_pointers.utils import make_good_unitary, get_heatmap_vectors, encode_point, power, \
-    get_heatmap_vectors_hex, encode_point_hex
+    get_heatmap_vectors_hex, encode_point_hex, get_heatmap_vectors_n
 import seaborn as sns
 
 parser = argparse.ArgumentParser('averaged heatmaps for regular 2D and hex encoding')
@@ -13,21 +13,21 @@ parser.add_argument('--dim', type=int, default=256)
 parser.add_argument('--limit', type=float, default=5)
 parser.add_argument('--res', type=int, default=256)
 parser.add_argument('--palette', type=str, default='plasma', choices=['default', 'diverging', 'plasma'])
+parser.add_argument('--n', type=int, default=3, help='projection dimension')
 
 args = parser.parse_args()
 
 if not os.path.exists('heatmap_output'):
     os.makedirs('heatmap_output')
 
-fname = 'heatmap_output/{}dim_{}seeds.npz'.format(args.dim, args.n_seeds)
+fname = 'heatmap_output/n{}_{}dim_{}seeds.npz'.format(args.n, args.dim, args.n_seeds)
 
 # square_axes = np.zeros((args.n_seeds, 2, args.dim))
 # hex_axes = np.zeros((args.n_seeds, 3, args.dim))
 
-axes = np.zeros((args.n_seeds, 3, args.dim))
+axes = np.zeros((args.n_seeds, args.n, args.dim))
 
-square_heatmaps = np.zeros((args.n_seeds, args.res, args.res))
-hex_heatmaps = np.zeros((args.n_seeds, args.res, args.res))
+heatmaps = np.zeros((args.n_seeds, args.res, args.res))
 
 # Origin vector is the same in all cases
 origin_vec = np.zeros((args.dim,))
@@ -39,40 +39,27 @@ ys = np.linspace(-args.limit, args.limit, args.res)
 # if the data already exists, just load it
 if os.path.exists(fname):
     data = np.load(fname)
-    square_heatmaps=data['square_heatmaps']
-    hex_heatmaps=data['hex_heatmaps']
+    heatmaps=data['heatmaps']
 else:
 
     for seed in range(args.n_seeds):
         print("\x1b[2K\r Seed {} of {}".format(seed + 1, args.n_seeds), end="\r")
-        rng = np.random.RandomState(seed=seed)
-        X = make_good_unitary(args.dim, rng=rng)
-        Y = make_good_unitary(args.dim, rng=rng)
-        Z = make_good_unitary(args.dim, rng=rng)
 
-        axes[seed, 0, :] = X.v
-        axes[seed, 1, :] = Y.v
-        axes[seed, 2, :] = Z.v
+        hmv, n_axes = get_heatmap_vectors_n(xs, ys, args.n, seed=seed, dim=args.dim)
 
-        square_heatmaps[seed, :, :] = np.tensordot(
+        heatmaps[seed, :, :] = np.tensordot(
             origin_vec,
-            get_heatmap_vectors(xs=xs, ys=ys, x_axis_sp=X, y_axis_sp=Y),
+            hmv,
             axes=([0], [2])
         )
 
-        hex_heatmaps[seed, :, :] = np.tensordot(
-            origin_vec,
-            get_heatmap_vectors_hex(xs=xs, ys=ys, x_axis_sp=X, y_axis_sp=Y, z_axis_sp=Z),
-            axes=([0], [2])
-        )
+        for i, axis in enumerate(n_axes):
+            axes[seed, i, :] = axis.v
 
     np.savez(
         fname,
-        # square_axes=square_axes,
-        # hex_axes=hex_axes,
         axes=axes,
-        square_heatmaps=square_heatmaps,
-        hex_heatmaps=hex_heatmaps,
+        heatmaps=heatmaps,
     )
 
 if args.palette == 'diverging':
@@ -87,14 +74,10 @@ elif args.palette == 'default':
 vmin = -1
 vmax = 1
 
-avg_square_heatmap = square_heatmaps.mean(axis=0)
-avg_hex_heatmap = hex_heatmaps.mean(axis=0)
+avg_heatmap = heatmaps.mean(axis=0)
 
 plt.figure()
-plt.imshow(avg_square_heatmap, vmin=vmin, vmax=vmax, cmap=cmap)
-plt.title("Square Heatmap")
-plt.figure()
-plt.imshow(avg_hex_heatmap, vmin=vmin, vmax=vmax, cmap=cmap)
-plt.title("Hexagonal Heatmap")
+plt.imshow(avg_heatmap, vmin=vmin, vmax=vmax, cmap=cmap)
+plt.title("Heatmap")
 
 plt.show()
---
+++ axis_vector_explorations/radial_deviation.py
@@ -0,0 +1,120 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import os
+from spatial_semantic_pointers.utils import make_good_unitary, get_heatmap_vectors, encode_point, power, \
+    get_heatmap_vectors_hex, encode_point_hex
+import seaborn as sns
+
+parser = argparse.ArgumentParser('radial deviation for regular 2D and hex encoding')
+
+parser.add_argument('--n-seeds', type=int, default=50)
+parser.add_argument('--dim', type=int, default=256)
+parser.add_argument('--res', type=int, default=128, help='resolution of radii used')
+parser.add_argument('--limit', type=float, default=5, help='highest radial distance to calculate')
+parser.add_argument('--samples', type=int, default=100, help='number of samples at each radius to randomly draw')
+
+args = parser.parse_args()
+
+
+def compute_rad_dev(X, Y, Z=None, res=128, limit=5, samples=100, hex=False):
+    rs = np.linspace(0, limit, res)
+
+    ret = np.zeros((res, samples))
+
+    # samples at random angles for each radius
+    angles = np.random.uniform(low=0, high=2 * np.pi, size=(res, samples))
+
+    for ir, r in enumerate(rs):
+
+        for i in range(samples):
+
+            # shouldn't matter which one is sin and cos here, angles are arbitrary
+            x = r * np.cos(angles[ir, i])
+            y = r * np.sin(angles[ir, i])
+            # dot product with the origin will just end up being value of the first element
+            if hex:
+                ret[ir, i] = encode_point_hex(x, y, X, Y, Z).v[0]
+            else:
+                ret[ir, i] = encode_point(x, y, X, Y).v[0]
+
+    return ret
+
+
+if not os.path.exists('rad_dev_output'):
+    os.makedirs('rad_dev_output')
+
+fname = 'rad_dev_output/{}dim_{}res_{}samples, {}seeds.npz'.format(args.dim, args.res, args.samples, args.n_seeds)
+
+# square_axes = np.zeros((args.n_seeds, 2, args.dim))
+# hex_axes = np.zeros((args.n_seeds, 3, args.dim))
+
+rad_dev_square = np.zeros((args.n_seeds, args.res, args.samples))
+rad_dev_hex = np.zeros((args.n_seeds, args.res, args.samples))
+
+axes = np.zeros((args.n_seeds, 3, args.dim))
+
+# if the data already exists, just load it
+if os.path.exists(fname):
+    data = np.load(fname)
+    rad_dev_square=data['rad_dev_square']
+    rad_dev_hex=data['rad_dev_hex']
+else:
+
+    for seed in range(args.n_seeds):
+        print("\x1b[2K\r Seed {} of {}".format(seed + 1, args.n_seeds), end="\r")
+        rng = np.random.RandomState(seed=seed)
+        X = make_good_unitary(args.dim, rng=rng)
+        Y = make_good_unitary(args.dim, rng=rng)
+        Z = make_good_unitary(args.dim, rng=rng)
+
+        axes[seed, 0, :] = X.v
+        axes[seed, 1, :] = Y.v
+        axes[seed, 2, :] = Z.v
+
+        rad_dev_square[seed, :, :] = compute_rad_dev(X, Y, Z=None, res=args.res, limit=args.limit, samples=args.samples, hex=False)
+
+        rad_dev_hex[seed, :, :] = compute_rad_dev(X, Y, Z, res=args.res, limit=args.limit, samples=args.samples, hex=True)
+
+    np.savez(
+        fname,
+        axes=axes,
+        rad_dev_square=rad_dev_square,
+        rad_dev_hex=rad_dev_hex,
+    )
+
+
+# vmin = None
+# vmax = None
+vmin = -1
+vmax = 1
+
+avg_rad_dev_square = rad_dev_square.mean(axis=2).mean(axis=0)
+avg_rad_dev_hex = rad_dev_hex.mean(axis=2).mean(axis=0)
+
+avg_abs_rad_dev_square = np.abs(rad_dev_square).mean(axis=2).mean(axis=0)
+avg_abs_rad_dev_hex = np.abs(rad_dev_hex).mean(axis=2).mean(axis=0)
+
+# plt.figure()
+# plt.plot(avg_rad_dev_square)
+# plt.title("Square Radial Deviation")
+# plt.figure()
+# plt.plot(avg_rad_dev_hex)
+# plt.title("Hexagonal Radial Deviation")
+# plt.figure()
+# plt.plot(avg_abs_rad_dev_square)
+# plt.title("Square Absolute Radial Deviation")
+# plt.figure()
+# plt.plot(avg_abs_rad_dev_hex)
+# plt.title("Hexagonal Absolute Radial Deviation")
+
+plt.figure()
+plt.plot(avg_rad_dev_square)
+plt.plot(avg_rad_dev_hex)
+plt.title("Radial Deviation")
+plt.figure()
+plt.plot(avg_abs_rad_dev_square)
+plt.plot(avg_abs_rad_dev_hex)
+plt.title("Absolute Radial Deviation")
+
+plt.show()
