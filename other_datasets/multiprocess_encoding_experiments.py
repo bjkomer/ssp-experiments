8b927548a9444e8dfc72c62a4827d850eaa98d2c
Brent Komer <brent.komer@gmail.com>
2020-03-25 16:17:45 -0400

    adding pc-gauss-tiled to command line options

--- other_datasets/multiprocess_encoding_experiments.py
+++ other_datasets/multiprocess_encoding_experiments.py
@@ -21,7 +21,7 @@ parser = argparse.ArgumentParser('Encoding experiment on PMLB')
 parser.add_argument('--debug', action='store_true', help='if set, just try on a few datasets and variants')
 parser.add_argument(
     '--encoding-type', type=str, default='all',
-    choices=['independent-ssp', 'combined-ssp', 'combined-simplex-ssp', 'all', 'pc-gauss', 'one-hot', 'tile-code'],
+    choices=['independent-ssp', 'combined-ssp', 'combined-simplex-ssp', 'all', 'pc-gauss', 'pc-gauss-tiled', 'one-hot', 'tile-code'],
     help='type of ssp encoding to use'
 )
 parser.add_argument('--sigma', type=float, default=0.5)
