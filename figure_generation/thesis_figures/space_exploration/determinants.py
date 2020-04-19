733efa4b8434bb37f009dfd30b07f8884a6fc758
Brent Komer <brent.komer@gmail.com>
2020-04-18 22:24:35 -0400

    more messing around with visualizations

---
+++ figure_generation/thesis_figures/space_exploration/determinants.py
@@ -0,0 +1,111 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+from scipy.linalg import circulant
+from spatial_semantic_pointers.utils import make_good_unitary
+import nengo.spa as spa
+
+# generate a pile of vectors
+# create circulant matrices out of them
+# only plot the points where the determinant is 1 or -1
+
+def unitary_determinant_test(dim=2, n_samples=1000):
+    for i in range(n_samples):
+        vec = make_good_unitary(dim=dim).v
+        assert np.allclose(np.abs(np.linalg.det(circulant(vec))), 1)
+    return True
+
+
+def sum_to_one_3d(res):
+    points = np.zeros((res**2, 3))
+    for i, x in enumerate(np.linspace(0, 1, res)):
+        for j, y in enumerate(np.linspace(0, 1 - x, res)):
+            z = 1 - x - y
+            points[i*res+j, :] = np.array([x, y, z])
+    return points
+
+
+def unitary_points(dim, n_samples, good_unitary=False):
+    points = np.zeros((n_samples, dim))
+    for i in range(n_samples):
+        if good_unitary:
+            points[i, :] = make_good_unitary(dim=dim).v
+        else:
+            sp = spa.SemanticPointer(dim)
+            sp.make_unitary()
+            points[i, :] = sp.v
+    return points
+
+
+# print(unitary_determinant_test())
+
+
+dim = 3
+res = 32
+n_samples = 50000
+
+# print(np.linalg.det(circulant(np.array([1./dim, 1./dim, 1./dim]))))
+
+limit = 1
+xs = np.linspace(-limit, limit, res)
+points = np.zeros((res**dim, dim))
+
+points = np.random.uniform(-limit, limit, size=(n_samples, dim))
+# points = unitary_points(dim, n_samples)
+# points = sum_to_one_3d(32)
+# n_samples = 32**2
+dets = np.zeros((n_samples,))
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+# eps = 0.001
+eps = 0.0001
+for i in range(n_samples):
+    mat = circulant(points[i, :])
+    dets[i] = np.linalg.det(mat)
+    # if np.abs(np.abs(dets[i]) - 1) < eps:
+    #     if dim == 2:
+    #         ax.scatter(points[i, 0], points[i, 1], color='blue', alpha=0.3)
+    #     else:
+    #         ax.scatter(points[i, 0], points[i, 1], points[i, 2], color='blue', alpha=0.3)
+
+
+inds = np.abs(np.abs(dets) - 1) < eps
+ax.scatter(points[inds, 0], points[inds, 1], points[inds, 2], color='blue', alpha=0.3)
+
+print(points[inds, :])
+
+# sto = sum_to_one_3d(16)
+# ax.scatter(sto[:, 0], sto[:, 1], sto[:, 2], color='red', alpha=0.3)
+
+# u = unitary_points(dim, 200)
+# ax.scatter(u[:, 0], u[:, 1], u[:, 2], color='green', alpha=0.8)
+
+
+vec = np.array([0.15568695,  0.96240234, - 0.48177102])
+print(np.linalg.norm(vec))
+# vec = vec / np.linalg.norm(vec)
+fft_vec = np.fft.fft(vec)
+print("det", np.linalg.det(circulant(vec)))
+print(fft_vec)
+print(fft_vec.real**2 + fft_vec.imag**2)
+rot = circulant(vec)
+n_samples = 100
+points = np.zeros((n_samples, dim))
+origin = np.ones(dim)
+origin[1] = 0
+points[0, 0] = 1
+for i in range(0, n_samples):
+    # points[i, :] = points[i - 1, :] @ rot
+    points[i, :] = np.fft.ifft(np.fft.fft(vec)**(i/10.)* np.fft.fft(origin)).real
+
+ax.scatter(points[:, 0], points[:, 1], points[:, 2], color='green', alpha=0.5)
+
+
+# fft_temp = np.zeros((3,), dtype='complex64')
+# fft_temp[0] = 1
+# fft_temp[1] = np.exp(1.j*np.pi/3.)
+# fft_temp[1] = np.exp(-1.j*np.pi/3.)
+
+plt.show()
--- figure_generation/thesis_figures/space_exploration/unitary_subspace_in_4d.py
+++ figure_generation/thesis_figures/space_exploration/unitary_subspace_in_4d.py
@@ -14,12 +14,17 @@ def unitary(phi_a, phi_b, sign=1):
     fv[2] = 1
     fv[3] = np.conj(fv[1])
 
+    # fv[0] = np.exp(1.j*np.pi/30)
+    # fv[1] = np.exp(1.j*phi_a)
+    # fv[2] = np.conj(fv[0])
+    # fv[3] = np.conj(fv[1])
+
     assert np.allclose(np.abs(fv), 1)
     v = np.fft.ifft(fv)
     # assert np.allclose(v.imag, 0, atol=1e-5)
     v = v.real
-    assert np.allclose(np.fft.fft(v), fv)
-    assert np.allclose(np.linalg.norm(v), 1)
+    # assert np.allclose(np.fft.fft(v), fv)
+    # assert np.allclose(np.linalg.norm(v), 1)
     return v
 
 
@@ -28,8 +33,6 @@ def random_sphere_points(n_samples=1000):
 
     for i in range(n_samples):
         surface[i, :] /= np.linalg.norm(surface[i, :])
-
-    # xyz = xyz / np.linalg.norm(xyz, axis=1) # doesnt work for some reason
 
     return surface
 
@@ -48,6 +51,30 @@ def unitary_from_sphere(n_samples=1000, dim=4):
     return surface
 
 
+def get_coord_rot_matrix(dim=4):
+    original_axes = np.eye(dim)
+    new_axes = np.zeros((dim, dim))
+    # this is the unit displacement vector of the hypersphere centers
+
+    # manually choosing a set that works
+    new_axes[0, :] = 1./np.sqrt(dim)
+
+    new_axes[1, 0] = new_axes[0, 1]
+    new_axes[1, 1] = -new_axes[0, 0]
+    new_axes[1, :] = new_axes[1, :] / np.linalg.norm(new_axes[1, :])
+
+    new_axes[2, 2] = new_axes[0, 1]
+    new_axes[2, 3] = -new_axes[0, 0]
+    new_axes[2, :] = new_axes[2, :]/ np.linalg.norm(new_axes[2, :])
+
+    new_axes[3, :2] = 1. / np.sqrt(dim)
+    new_axes[3, 2:] = -1. / np.sqrt(dim)
+
+    print(np.linalg.det(new_axes))
+
+    return new_axes
+
+coord_rot = get_coord_rot_matrix()
 
 n_samples = 6
 eps = 0.001
@@ -61,10 +88,10 @@ palette = sns.color_palette("hls", n_samples)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-res = 256#32#256#64
+res = 128#32#32#256#64
 # xs = np.linspace(-5, 5, res)
 xs = np.linspace(0, 24, res)
-xs = np.linspace(0, 24, res)
+xs = np.linspace(0, 100, res)
 
 #signs = [1, -1]
 signs = [1]
@@ -73,9 +100,11 @@ phis = [np.pi/3., np.pi/4.]
 
 palette = sns.color_palette("hls", len(phis)**2)
 
-if True:
+if False:
     surface = random_sphere_points(n_samples=1000)
-    ax.scatter(surface[:, 0], surface[:, 1], surface[:, 2], color='blue', alpha=0.1)
+    surface = surface @ coord_rot
+    # ax.scatter(surface[:, 0], surface[:, 1], surface[:, 2], color='blue', alpha=0.1)
+    ax.scatter(surface[:, 1], surface[:, 2], surface[:, 3], color='blue', alpha=0.1)
 #
 # if True:
 #     ax.plot([0, 1], [0, 0], [0, 0], color='green')
@@ -99,7 +128,10 @@ if ssp_manifold == 1:
                 #         ax.scatter(ux[0], ux[1], ux[2], color=palette[i])
                 for x in xs:
                     ux = np.fft.ifft(np.fft.fft(u) ** x).real
-                    ax.scatter(ux[0], ux[1], ux[2], color=palette[i*len(phis) + j])
+                    ax.scatter(ux[1], ux[2], ux[3], color=palette[i * len(phis) + j])
+                    # ux = ux @ coord_rot
+                    # # ax.scatter(ux[0], ux[1], ux[2], color=palette[i*len(phis) + j])
+                    # ax.scatter(ux[1], ux[2], ux[3], color=palette[i * len(phis) + j])
                     # print(np.linalg.norm(ux))
                 #     # optional imaginary part, only nonzero if sign is -1
                 #     # ux = np.fft.ifft(np.fft.fft(u) ** x).imag
