d3f83c6f1f861b26ed85971fb37d230fdd1ea532
Brent Komer <brent.komer@gmail.com>
2020-08-05 16:14:18 -0400

    plotting more results

--- rl_ssp/training_figure.py
+++ rl_ssp/training_figure.py
@@ -15,6 +15,28 @@ fnames = [
 #     'models_tensorflow/large_td3_256dim_2048x2_0sensors_4seed_5000000steps_0gd_100ms_cur_scaling0.5.npz',
 # ]
 
+fnames = [
+    'models_tensorflow/medium_td3_256dim_2048x2_0sensors_70seed_500000steps_0gd_100ms_scaling0.5.npz',
+    'models_tensorflow/medium_td3_256dim_2048x2_0sensors_71seed_500000steps_0gd_100ms_scaling0.5.npz',
+    'models_tensorflow/medium_td3_256dim_2048x2_0sensors_72seed_500000steps_0gd_100ms_scaling0.5.npz',
+]
+
+fnames = [
+    'models_tensorflow/medium_td3_256dim_2048x2_0sensors_70seed_500000steps_0gd_100ms_cur_scaling0.5.npz',
+    'models_tensorflow/medium_td3_256dim_2048x2_0sensors_71seed_500000steps_0gd_100ms_cur_scaling0.5.npz',
+    # 'models_tensorflow/medium_td3_256dim_2048x2_0sensors_72seed_500000steps_0gd_100ms_cur_scaling0.5.npz',
+]
+
+fnames = [
+    'models_tensorflow/medium_td3_256dim_2048x2_0sensors_70seed_500000steps_0gd_100ms_scaling0.5.npz',
+    'models_tensorflow/medium_td3_256dim_2048x2_0sensors_71seed_500000steps_0gd_100ms_scaling0.5.npz',
+    'models_tensorflow/medium_td3_256dim_2048x2_0sensors_72seed_500000steps_0gd_100ms_scaling0.5.npz',
+
+    'models_tensorflow/medium_td3_256dim_2048x2_0sensors_70seed_500000steps_0gd_100ms_cur_scaling0.5.npz',
+    'models_tensorflow/medium_td3_256dim_2048x2_0sensors_71seed_500000steps_0gd_100ms_cur_scaling0.5.npz',
+    # 'models_tensorflow/medium_td3_256dim_2048x2_0sensors_72seed_500000steps_0gd_100ms_cur_scaling0.5.npz',
+]
+
 for fname in fnames:
     data = np.load(fname)['eval_data']
     plt.plot(data[:, 0])
